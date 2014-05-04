c81e4ff63d0ebd5110123eea29c6efa1f4827be5
grromrell <grromrell@gmail.com>
2014-05-03 20:10:36 -0500

    Finished Lovins Stemmer

--- base_nlp/stemmers/lovins.py
+++ base_nlp/stemmers/lovins.py
@@ -1,14 +1,21 @@
+import re
+
 class LovinsStemmer(object):
     def __init__(self):
-        self.word = word
+        self.word = None
 
-    def stem(self):
+    def stem(self, word):
+        self.word = word
+        self.stem_rules()
+        self.recode()
         yield self.word
 
-    def stemming(self):
+    def stem_rules(self):
+        suffix = ''
         suffix_list = [end for end in SUFFIXES if self.word.endswith(end)]
         if suffix_list:
             suffix = max(suffix_list, key=len)
+        if len(self.word.replace(suffix, '')) >= 2 and suffix:
             if suffix in SUFFIXES[:203]:
                 self.word = self.word.replace(suffix, '')
             elif suffix in SUFFIXES[203:232]:
@@ -42,7 +49,166 @@ class LovinsStemmer(object):
                     self.word = self.word.replace(suffix, '')
             elif suffix in SUFFIXES[268:269]:
                 if len(self.word.replace(suffix, '')) >= 3:
+                    if self.word.replace(suffix, '')[-1] in ['l', 'i']:
+                        if re.search(r'u*e', self.word.replace(suffix, ''))[-3]:
+                            self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[269:271]:
+                if self.word.replace(suffix, '')[-2] == 'os':
+                    self.word = self.word.replace(suffix, '')
+                elif self.word.replace(suffix, '')[-1] not in ['u', 'x', 's']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[271:273]:
+                if self.word.replace(suffix, '')[-1] not in ['a', 'c', 'e', 'm']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[273:275]:
+                if re.search(r's**', self.word.replace(suffix, '')[-3]):
+                    if len(self.word.replace(suffix, '')) >= 4:
+                        self.word = self.word.replace(suffix, '')
+                elif len(self.word.replace(suffix, '')) >= 3:
+                    self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[275:276]:
+                if self.word.replace(suffix, '')[-1] in ['l', 'i']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[276:277]:
+                if self.word.replace(suffix, '')[-1] != 'c':
+                    self.word = word.replace(suffix, '')
+            elif suffix in SUFFIXES[277:278]:
+                if len(self.word.replace(suffix, '')) >= 3:
+                    if self.word.replace(suffix, '')[-1] not in ['l', 'n']:
+                        self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[278:280]:
+                if self.word.replace(suffix, '')[-1] in ['n', 'r']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[280:281]:
+                if self.word.replace(suffix, '')[-2] == 'tt':
+                    pass
+                elif self.word.replace(suffix, '')[-2] == 'dr':
+                    self.word = self.word.replace(suffix, '')
+                elif self.word.replace(suffix, '')[-1] == 't':
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[281:282]:
+                if self.word.replace(suffix, '')[-1] in ['s', 't']:
+                    if self.word.replace(suffix, '')[-2] == 'ot':
+                        pass
+                    else:
+                        self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[282:283]:
+                if self.word.replace(suffix, '')[-1] in ['l', 'm', 'n', 'r']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[283:284]:
+                if self.word.replace(suffix, '')[-1] == 'c':
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[284:285]:
+                if self.word.replace(suffix, '')[-1] not in ['s', 'u']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[285:286]:
+                if self.word.replace(suffix, '')[-1] in ['l', 'i']:
+                    self.word = self.word.replace(suffix, '')
+                elif re.search(r'u*e', word.replace(suffix, '')[-3]):
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[286:290]:
+                if self.word.replace(suffix, '')[-2] == 'in':
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[290:291]:
+                if self.word.replace(suffix, '')[-1] != 'f':
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[291:292]:
+                if self.word.replace(suffix, '')[-1] in ['d','f', 'l', 't']:
+                    self.word = self.word.replace(suffix, '')
+                elif self.word.replace(suffix, '')[-2] in ['ph', 'th', 'er', 
+                                                           'or', 'es']:
+                    self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[292:295]:
+                if len(self.word.replace(suffix, '')) >= 3:
+                    if self.word.replace(suffix, '')[-3] not in ['met']:
+                        if self.word.replace(suffix, '')[-4] not in ['ryst']:
+                            self.word = self.word.replace(suffix, '')
+            elif suffix in SUFFIXES[295:296]:
+                if self.word.replace(suffix, '')[-1] == 'l':
+                    self.word = self.word.replace(suffix, '')
 
+    def recode(self):
+        if any(self.word.endswith(x) for x in ['bb', 'dd', 'gg', 'll', 'mm', 
+                                               'nn', 'nn', 'pp', 'rr', 'ss',
+                                               'tt',]):
+            self.word = self.word[:-1]
+        elif self.word.endswith('ul'):
+            if self.word[-3] not in ['a', 'o', 'i']:
+                self.word = self.word.replace('ul', 'l')
+        elif self.word.endswith('end'):
+            if self.word[-4] != 's':
+                self.word = self.word.replace('end', 'ens')
+        elif self.word.endswith('her'):
+            if self.word[-4] not in ['p', 't']:
+                self.word = self.word.replace('her', 'hes')
+        elif self.word.endswith('ent'):
+            if self.word[-4] != 'm':
+                self.word = self.word.replace('ent', 's')
+        elif self.word.endswith('et'):
+            if self.word[-3] != 'n':
+                self.word = self.word.replace('et', 'es')
+        else:
+            suffix = ''
+            suffix_list = [end for end in RECODES if self.word.endswith(end)]
+            if suffix_list:
+                suffix = max(suffix_list, key=len)
+            if suffix:
+                if suffix == 'iev':
+                    self.word = self.word.replace(suffix, 'ief')
+                elif suffix == 'uct':
+                    self.word = self.word.replace(suffix, 'uc')
+                elif suffix == 'umpt':
+                    self.word = self.word.replace(suffix, 'um')
+                elif suffix == 'rpt':
+                    self.word = self.word.replace(suffix, 'rb')
+                elif suffix == 'urs':
+                    self.word = self.word.replace(suffix, 'ur')
+                elif suffix == 'istr':
+                    self.word = self.word.replace(suffix, 'ister')
+                elif suffix == 'metr':
+                    self.word = self.word.replace(suffix, 'meter')
+                elif suffix == 'olv':
+                    self.word = self.word.replace(suffix, 'olut')
+                elif suffix == 'bex':
+                    self.word = self.word.replace(suffix, 'bic')
+                elif suffix == 'dex':
+                    self.word = self.word.replace(suffix, 'dic')
+                elif suffix == 'pex':
+                    self.word = self.word.replace(suffix, 'pic')
+                elif suffix == 'tex':
+                    self.word = self.word.replace(suffix, 'tic')
+                elif suffix == 'ax':
+                    self.word = self.word.replace(suffix, 'ac')
+                elif suffix == 'ex':
+                    self.word = self.word.replace(suffix, 'ec')
+                elif suffix == 'ix':
+                    self.word = self.word.replace(suffix, 'ic')
+                elif suffix == 'lux':
+                    self.word = self.word.replace(suffix, 'luc')
+                elif suffix == 'uad':
+                    self.word = self.word.replace(suffix, 'uas')
+                elif suffix == 'vad':
+                    self.word = self.word.replace(suffix, 'vas')
+                elif suffix == 'cid':
+                    self.word = self.word.replace(suffix, 'cis')
+                elif suffix == 'lid':
+                    self.word = self.word.replace(suffix, 'lis')
+                elif suffix == 'erid':
+                    self.word = self.word.replace(suffix, 'eris')
+                elif suffix == 'pand':
+                    self.word = self.word.replace(suffix, 'pans')
+                elif suffix == 'ond':
+                    self.word = self.word.replace(suffix, 'ons')
+                elif suffix == 'lud':
+                    self.word = self.word.replace(suffix, 'lus')
+                elif suffix == 'rud':
+                    self.word = self.word.replace(suffix, 'rus')
+                elif suffix == 'mit':
+                    self.word = self.word.replace(suffix, 'mis')
+                elif suffix == 'ert':
+                    self.word = self.word.replace(suffix, 'ers')
+                elif suffix in ['yt', 'ys']:
+                    self.word = self.word.replace(suffix, 'ys')
 
 SUFFIXES = ['arizability', 'antialness', 'arisations', 'arizations',
             'entialness', 'antaneous', 'antiality', 'arisation', 'arization',
@@ -88,3 +254,8 @@ SUFFIXES = ['arizability', 'antialness', 'arisations', 'arizations',
             'ars', 'ies', 'ion', 'one', 'yl', 'on', 'or', 'um', 'us', 's', 
             'ar', 'early', 'ealy', 'eal', 'ear', 'eature', 'ite', 'allic', 
             'als', 'al', 'inity']
+
+RECODES = ['iev', 'uct', 'umpt', 'rpt', 'urs', 'istr', 'metr', 'olv', 'bex', 
+           'dex', 'pex', 'tex', 'ax', 'ex', 'ix', 'lux', 'uad', 'vad', 'cid',
+           'lid', 'erid', 'pand', 'ond', 'lud', 'rud', 'mit', 'ert', 'yt', 
+           'yz']
